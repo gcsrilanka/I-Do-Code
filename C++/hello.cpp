92a1f97c3d72f2aeeb955f3ad4e254ef3ba4f903
Victor Hugo M. Pinto <victor2142@gmail.com>
2018-10-12 12:44:50 -0300

    Add interactive Hello World in C++

---
+++ C++/hello.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+int main () {
+    string your_name;
+    cout<<"Please give me your name so that I can greet you :)"<<endl;
+    cin >> your_name;
+
+    cout << "Hello, " << your_name << "!" << endl;
+    return 0;
+}
