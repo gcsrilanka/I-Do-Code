7b0a7147ba0e8d0128cb39378e1d64c5343d159d
Snehit <redarcher9@Mindspace2.0>
2018-10-12 18:14:39 +0530

    added for loop for javascript

---
+++ Javascript/for_loop/for_loop.js
@@ -0,0 +1,1 @@
+for( var i =0;i<5;i++) { console.log(i) } #run this code on console of web browser
--- python/for_loop/for_loop.py
+++
@@ -1,3 +0,0 @@
-for i in range(0,5):  #Executes the statements inside the loop 5 times
-    print(i) #Identation marks the begining of the loop
-    print("hello world") 
