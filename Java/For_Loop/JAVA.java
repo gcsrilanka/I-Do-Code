a30ab487c73a4e14821398455195827e15724fc8
rashmiee <rashmiprabha1997@gmail.com>
2018-10-17 22:08:45 +0530

    I added asteric pattern(Pyramid pattern) using for loop in java

---
+++ Java/For_Loop/JAVA.java
@@ -0,0 +1,42 @@
+class JAVA
+{
+public static void main(String []args)
+	{
+	print();
+	}
+	public static void print()
+	{
+	for(int k=0;k<10;k++)
+	{
+		for(int l=10;l>k;l--)
+		{
+		System.out.print("* ");
+		}
+		for(int l=0;l<=k;l++)
+		{
+		System.out.print(" ");
+		}
+		for(int l=0;l<=k;l++)
+		{
+		System.out.print(" ");
+		}
+		for(int l=0;l<=k;l++)
+		{
+		System.out.print(" ");
+		}
+		for(int l=0;l<=k;l++)
+		{
+		System.out.print(" ");
+		}
+		for(int l=10;l>k;l--)
+		{
+		System.out.print("* ");
+		}
+	System.out.println();
+
+
+}
+}
+}
+
+
