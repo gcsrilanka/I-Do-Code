77ec854fcb656d4bd6a49860e3ede8181d2f76fd
Priyanka Negi <prinegi100@gmail.com>
2018-10-28 10:17:24 +0530

    switch case using js

---
+++ JavaScript-NodeJs/switch-block.js
@@ -0,0 +1,28 @@
+function switchStatements (num) {
+
+  switch(num) {
+    case 1:
+    console.log("1");
+    break;
+    case 2: 
+    console.log("2");
+    break;
+    case 3: 
+    console.log("3");
+    break;
+    case 4: 
+    console.log("4");
+    break;
+    case 5: 
+    console.log("5");
+    break;
+
+    default : 
+    console.log("not found");
+    break;
+
+  }
+
+}
+
+switchStatements(4);
