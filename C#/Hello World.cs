ab8543210a515cdb2aff9c8f683bfaec5c0bffa7
Thisuri090 <tguruge456@gmail.com>
2018-10-29 18:50:19 +0530

    Added Hello World in C#

---
+++ C#/Hello World.cs
@@ -0,0 +1,16 @@
+ Hello World in C#
+
+using System;
+namespace HelloWorld
+{
+    class Hello 
+    {
+        static void Main() 
+        {
+            Console.WriteLine("Hello World");
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
