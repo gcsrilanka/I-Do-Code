994c9733a47e3ea2538d43ad983823c2b3625a1a
thisuri090 <tguruge456@gmail.com>
2019-10-11 09:04:59 +0530

    Hello World

--- C#/HelloWorld.cs
+++ C#/HelloWorld.cs
@@ -6,7 +6,7 @@ namespace HelloWorld
     {
         static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World... Welcome to Hacktoberfest!");
             Console.ReadKey()
         }
     }
