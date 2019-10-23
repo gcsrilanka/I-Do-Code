370f063f6981b1fe9c8ad2e5ae4e5c0b5c639919
Sithira Akbo <56906001+dlsakbo@users.noreply.github.com>
2019-10-23 19:33:07 +0530

    Change no of repitition

--- perl/for_loop/hello_world_for_loop.pl
+++ perl/for_loop/hello_world_for_loop.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
  
 # Hello World in for loop
-for( $count = 0; $count < 5; $count = $count + 1 ) {
+for( $count = 0; $count < 5000; $count = $count + 1 ) {
    print "Hello World: $count\n";
 }
