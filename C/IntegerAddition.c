7b63138c0b93284424db2c944a6e2da94bd24c4c
esiriwardane97 <72193850+esiriwardane97@users.noreply.github.com>
2020-10-07 12:38:21 +0530

    adding

--- C/IntegerAddition.c
+++ C/IntegerAddition.c
@@ -4,6 +4,12 @@ int main()
   int a,b;
   printf("enter two integer values to add:");
   scanf("%d %d",&a,&b);
+  
+  int c,d;
+  printf("enter two integer values to add:");
+  scanf("%d %d",&c,&d);
 
   printf("%d + %d =%d",a,b,(a+b));
+  
+  printf("%d + %d =%d",c,d,(c+d));
 }
