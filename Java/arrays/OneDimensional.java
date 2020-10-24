4b34a89d7fb66f90ba83c503c43f0c7dd915665e
nawodyaonline <nawodyahckr@gmail.com>
2020-10-24 18:53:44 +0530

    Added one dimensional array example

---
+++ Java/arrays/OneDimensional.java
@@ -0,0 +1,14 @@
+package arrays;
+
+public class OneDimensional {
+    public static void main(String[] args) {
+        int[] number = new int[3];
+        number[0] = 10;
+        number[1] = 20;
+        number[2] = 30;
+
+        System.out.println(number[0]);
+        System.out.println(number[1]);
+        System.out.println(number[2]);
+    }
+}
