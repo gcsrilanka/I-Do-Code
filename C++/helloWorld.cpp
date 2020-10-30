c650b7c8289fb53d5f0bda2204b1f466e2b984a0
Navinda Lankesh <47824016+paradocx96@users.noreply.github.com>
2020-10-30 20:56:19 +0530

    Added C++ HelloWorld!

---
+++ C++/helloWorld.cpp
@@ -0,0 +1,13 @@
+// Header file for input output functions 
+#include<iostream> 
+
+using namespace std;
+
+// main function
+int main() 
+{ 
+	// prints hello world 
+	cout << "Hello World"; 
+	
+	return 0; 
+}
